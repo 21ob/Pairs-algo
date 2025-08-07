6c377267a37f2d8427520bbe96167da87a57cb43
21ob <37551296+21ob@users.noreply.github.com>
2025-08-07 10:38:40 +0100

    Add files via upload

--- Trader.py
+++ Trader.py
@@ -11,6 +11,7 @@ class TradingEngine:
         trade_count (int): Number of executed trades.
         portfolio_tracker (array): Track total portfolio value (balance + unrealized P/L).
         returns (array): Track returns.
+        trade_log (2d array): Tracks date, direction, price and quantity of all trades executed by strategy
     """
 
     def __init__(self, balance, position=0):
@@ -20,9 +21,22 @@ class TradingEngine:
         self.trade_count = 0
         self.portfolio_tracker = []
         self.returns = []
-        
+        self.trade_log = []
+
     
-    def sell(self, price, quantity):
+    def _log_trade(self, date, action, price, quantity):
+        """Internal method to log each trade."""
+        self.trade_log.append({
+            "date": pd.to_datetime(date),
+            "action": action,
+            "price": price,
+            "quantity": quantity,
+            "position": self.position,
+            "balance": self.balance
+        })
+
+    
+    def sell(self, price, quantity, date):
         """
         Executes a sell trade and updates balance and position.
     
@@ -34,9 +48,10 @@ class TradingEngine:
         self.balance -= price * quantity
         self.position += quantity
         self.trade_count += 1
+        self._log_trade(date, "SELL", price, quantity)
         
     
-    def buy(self, price, quantity):
+    def buy(self, price, quantity, date):
         """
         Executes a buy trade and updates balance and position.
         
@@ -48,9 +63,10 @@ class TradingEngine:
         self.balance -= price * quantity
         self.position += quantity
         self.trade_count += 1
+        self._log_trade(date, "BUY", price, quantity)
         
     
-    def close_position(self, price):
+    def close_position(self, price, date):
         """
         Executes a buy or sell trade to completely close net position to zero.
         
@@ -58,9 +74,15 @@ class TradingEngine:
             price (float): Price of the asset.
         """        
         self.balance += self.position * price
+        self._log_trade(date, "CLOSE", price, self.position)
         self.position = 0
-        
+
     
+    def get_trade_log(self):
+        """Returns the trade log as a pandas DataFrame."""
+        return pd.DataFrame(self.trade_log)
+
+        
     def run_strategy(
         self, 
         buy_signal, 
@@ -68,7 +90,7 @@ class TradingEngine:
         price, 
         close_price, 
         dates, 
-        allow_negative_balance=True):
+        allow_negative_balance=False):
         """
         Iterates over signals and executes trades accordingly. Position exit based on mean reversion.
     
@@ -89,25 +111,25 @@ class TradingEngine:
         for i in range(timesteps):
             if allow_negative_balance:
                 if buy_signal[i]:
-                    self.buy(price=price[i], quantity=buy_signal[i])
+                    self.buy(price=price[i], quantity=buy_signal[i], date=dates[i])
                     
                 elif sell_signal[i]:
-                    self.sell(price=price[i], quantity=sell_signal[i])
+                    self.sell(price=price[i], quantity=sell_signal[i], date=dates[i])
             
                 elif (((self.position > 0) & (price[i] > close_price[i])) | ((self.position < 0) & (price[i] < close_price[i]))):
-                    self.close_position(price[i])
+                    self.close_position(price[i], date=dates[i])
 
             else:
-                if self.balance == 0:
+                if self.balance < 0:
                     break
                 elif buy_signal[i]:
-                    self.buy(price=price[i], quantity=buy_signal[i])
+                    self.buy(price=price[i], quantity=buy_signal[i], date=dates[i])
                     
                 elif sell_signal[i]:
-                    self.sell(price=price[i], quantity=sell_signal[i])
+                    self.sell(price=price[i], quantity=sell_signal[i], date=dates[i])
             
                 elif (((self.position > 0) & (price[i] > close_price[i])) | ((self.position < 0) & (price[i] < close_price[i]))):
-                    self.close_position(price[i])
+                    self.close_position(price[i], date=dates[i])
 
 
             # Track portfolio value and returns
@@ -119,6 +141,7 @@ class TradingEngine:
             portfolio_prev = portfolio_value
             self.position_tracker.append(self.position)
 
-        self.close_position(price[-1])  #Close any outstanding positions
+        # Final close
+        self.close_position(price[i], dates[i])
         
-        return pd.Series(self.returns, index=pd.to_datetime(dates))
+        return pd.Series(self.returns, index=pd.to_datetime(dates[:i]))
